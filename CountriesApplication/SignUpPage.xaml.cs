df49ff46ac0a553ec97bbe618e690dfadcabe3c2
noam9472-gif <noam9472@gmail.com>
2026-01-04 15:45:30 +0200

    Add project files.

---
+++ CountriesApplication/SignUpPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CountriesApplication
+{
+    public partial class SignUpPage : Page
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmailBox.Text != ConfirmEmailBox.Text)
+            {
+                MessageBox.Show("Emails do not match ❌");
+                return;
+            }
+
+            MessageBox.Show("Account created successfully ✅");
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
